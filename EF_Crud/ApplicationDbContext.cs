d5bd93aa9d6efcadbe869290996b791935332814
MihailCoscodan <mihail.coscodan@acconomy.com>
2022-03-28 19:48:04 +0300

    First comit, crud example

---
+++ EF_Crud/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EF_Crud.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EF_Crud
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Server=.;Database=UserDB;Trusted_connection=True;");
+        }
+    }
+}
---
+++ EF_Crud/Program.cs
@@ -0,0 +1,49 @@
+﻿using EF_Crud.Entities;
+using System;
+using System.Linq;
+
+namespace EF_Crud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UseEF();
+            Console.Read();
+        }
+
+        private static void UseEF()
+        {
+            using (ApplicationDbContext dbContext = new ApplicationDbContext())
+            {
+                //// create new users
+                //var girl = new User() { Name = "Cleopatra", Age = 23 };
+                //var boy = new User() { Name = "Cesar", Age = 32 };
+
+                //// add user into db (insert)
+                //dbContext.Users.Add(girl);
+                //dbContext.Users.Add(boy);
+
+                // get all users
+                var users = dbContext.Users.ToList();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Id} {user.Name} {user.Age}");
+                }
+                ///Update (get user)
+
+                var cleo = dbContext.Users.FirstOrDefault(x => x.Name == "Cleopatra");
+                cleo.Age = 21;
+
+                // Delete
+                User userToDelete = dbContext.Users.FirstOrDefault(x => x.Id == 1020);
+                dbContext.Users.Remove(userToDelete);
+
+                dbContext.SaveChanges();
+
+            }
+        }
+    }
+}
